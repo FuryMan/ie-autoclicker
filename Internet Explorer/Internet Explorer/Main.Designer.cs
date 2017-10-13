06a7192d9d1347f76487795e4338c6e987468e92
skidfury <30561958+Kansler@users.noreply.github.com>
2017-10-13 14:39:19 -0500

    Add files via upload

---
+++ Internet Explorer/Internet Explorer/Main.Designer.cs
@@ -0,0 +1,164 @@
+﻿namespace Internet_Explorer
+{
+    partial class Main
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Main));
+            this.materialRaisedButton1 = new MaterialSkin.Controls.MaterialRaisedButton();
+            this.trackBar1 = new System.Windows.Forms.TrackBar();
+            this.materialRaisedButton2 = new MaterialSkin.Controls.MaterialRaisedButton();
+            this.materialLabel1 = new MaterialSkin.Controls.MaterialLabel();
+            this.Autoclicker = new System.Windows.Forms.Timer(this.components);
+            this.CheckIE = new System.Windows.Forms.Timer(this.components);
+            this.CheckKeyState = new System.Windows.Forms.Timer(this.components);
+            this.materialRaisedButton3 = new MaterialSkin.Controls.MaterialRaisedButton();
+            this.Hiding = new System.Windows.Forms.Timer(this.components);
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // materialRaisedButton1
+            // 
+            this.materialRaisedButton1.Depth = 0;
+            this.materialRaisedButton1.Location = new System.Drawing.Point(108, 150);
+            this.materialRaisedButton1.MouseState = MaterialSkin.MouseState.HOVER;
+            this.materialRaisedButton1.Name = "materialRaisedButton1";
+            this.materialRaisedButton1.Primary = true;
+            this.materialRaisedButton1.Size = new System.Drawing.Size(108, 36);
+            this.materialRaisedButton1.TabIndex = 0;
+            this.materialRaisedButton1.Text = "Toggle on";
+            this.materialRaisedButton1.UseVisualStyleBackColor = true;
+            this.materialRaisedButton1.Click += new System.EventHandler(this.materialRaisedButton1_Click);
+            // 
+            // trackBar1
+            // 
+            this.trackBar1.BackColor = System.Drawing.Color.White;
+            this.trackBar1.Location = new System.Drawing.Point(12, 99);
+            this.trackBar1.Maximum = 200;
+            this.trackBar1.Minimum = 10;
+            this.trackBar1.Name = "trackBar1";
+            this.trackBar1.Size = new System.Drawing.Size(311, 45);
+            this.trackBar1.TabIndex = 1;
+            this.trackBar1.TickFrequency = 10;
+            this.trackBar1.Value = 10;
+            this.trackBar1.Scroll += new System.EventHandler(this.trackBar1_Scroll);
+            // 
+            // materialRaisedButton2
+            // 
+            this.materialRaisedButton2.Depth = 0;
+            this.materialRaisedButton2.Location = new System.Drawing.Point(108, 232);
+            this.materialRaisedButton2.MouseState = MaterialSkin.MouseState.HOVER;
+            this.materialRaisedButton2.Name = "materialRaisedButton2";
+            this.materialRaisedButton2.Primary = true;
+            this.materialRaisedButton2.Size = new System.Drawing.Size(108, 36);
+            this.materialRaisedButton2.TabIndex = 2;
+            this.materialRaisedButton2.Text = "Unload";
+            this.materialRaisedButton2.UseVisualStyleBackColor = true;
+            this.materialRaisedButton2.Click += new System.EventHandler(this.materialRaisedButton2_Click);
+            // 
+            // materialLabel1
+            // 
+            this.materialLabel1.AutoSize = true;
+            this.materialLabel1.BackColor = System.Drawing.Color.Transparent;
+            this.materialLabel1.Depth = 0;
+            this.materialLabel1.Font = new System.Drawing.Font("Roboto", 11F);
+            this.materialLabel1.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(222)))), ((int)(((byte)(0)))), ((int)(((byte)(0)))), ((int)(((byte)(0)))));
+            this.materialLabel1.Location = new System.Drawing.Point(105, 77);
+            this.materialLabel1.MouseState = MaterialSkin.MouseState.HOVER;
+            this.materialLabel1.Name = "materialLabel1";
+            this.materialLabel1.Size = new System.Drawing.Size(111, 19);
+            this.materialLabel1.TabIndex = 3;
+            this.materialLabel1.Text = "Average CPS: 1";
+            // 
+            // Autoclicker
+            // 
+            this.Autoclicker.Tick += new System.EventHandler(this.Autoclicker_Tick);
+            // 
+            // CheckIE
+            // 
+            this.CheckIE.Enabled = true;
+            this.CheckIE.Interval = 1000;
+            this.CheckIE.Tick += new System.EventHandler(this.CheckIE_Tick);
+            // 
+            // materialRaisedButton3
+            // 
+            this.materialRaisedButton3.Depth = 0;
+            this.materialRaisedButton3.Location = new System.Drawing.Point(108, 192);
+            this.materialRaisedButton3.MouseState = MaterialSkin.MouseState.HOVER;
+            this.materialRaisedButton3.Name = "materialRaisedButton3";
+            this.materialRaisedButton3.Primary = true;
+            this.materialRaisedButton3.Size = new System.Drawing.Size(108, 36);
+            this.materialRaisedButton3.TabIndex = 4;
+            this.materialRaisedButton3.Text = "Hide";
+            this.materialRaisedButton3.UseVisualStyleBackColor = true;
+            this.materialRaisedButton3.Click += new System.EventHandler(this.materialRaisedButton3_Click);
+            // 
+            // Hiding
+            // 
+            this.Hiding.Interval = 25;
+            this.Hiding.Tick += new System.EventHandler(this.Hiding_Tick);
+            // 
+            // Main
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(335, 280);
+            this.Controls.Add(this.materialRaisedButton3);
+            this.Controls.Add(this.materialLabel1);
+            this.Controls.Add(this.materialRaisedButton2);
+            this.Controls.Add(this.trackBar1);
+            this.Controls.Add(this.materialRaisedButton1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "Main";
+            this.ShowInTaskbar = false;
+            this.Sizable = false;
+            this.Text = "Internet Explorer";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.Main_FormClosing);
+            this.Load += new System.EventHandler(this.Main_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private MaterialSkin.Controls.MaterialRaisedButton materialRaisedButton1;
+        private System.Windows.Forms.TrackBar trackBar1;
+        private MaterialSkin.Controls.MaterialRaisedButton materialRaisedButton2;
+        private MaterialSkin.Controls.MaterialLabel materialLabel1;
+        private System.Windows.Forms.Timer Autoclicker;
+        private System.Windows.Forms.Timer CheckIE;
+        private System.Windows.Forms.Timer CheckKeyState;
+        private MaterialSkin.Controls.MaterialRaisedButton materialRaisedButton3;
+        private System.Windows.Forms.Timer Hiding;
+    }
+}
+
---
+++ Internet Explorer/Internet Explorer/Main.cs
@@ -0,0 +1,170 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using MaterialSkin;
+using MaterialSkin.Controls;
+using System.Diagnostics;
+using System.Runtime.InteropServices;
+using System.Windows.Input;
+
+namespace Internet_Explorer
+{
+    public partial class Main : MaterialSkin.Controls.MaterialForm
+    {
+        public static int toggle = 0;
+        public static int toggle1 = 0;
+
+        [DllImport("user32.dll", CharSet = System.Runtime.InteropServices.CharSet.Auto, CallingConvention = System.Runtime.InteropServices.CallingConvention.StdCall)]
+        public static extern void mouse_event(uint dwFlags, uint dx, uint dy, uint cButtons, uint dwExtraInfo);
+
+        [DllImport("user32.dll")]
+        private static extern short GetAsyncKeyState(Keys key);
+
+        Random random = new Random();
+        private const int MOUSEEVENTF_LEFTDOWN = 0x02;
+        private const int MOUSEEVENTF_LEFTUP = 0x04;
+        private const int MOUSEEVENTF_RIGHTDOWN = 0x08;
+        private const int MOUSEEVENTF_RIGHTUP = 0x10;
+
+        Process[] proc = Process.GetProcessesByName("iexplore");
+
+        public Main()
+        {
+            InitializeComponent();
+        }
+
+        public virtual double nextGaussian()
+        {
+            Random rand = new Random();
+            double v1 = 0;
+            double v2 = 0;
+            double s = 0;
+            do
+            {
+                v1 = 2 * rand.NextDouble() - 1;
+                v2 = 2 * rand.NextDouble() - 1;
+                s = v1 * v1 + v2 * v2;
+            } while (s >= 1 || s == 0);
+
+            double multiplier = Math.Sqrt(-2 * Math.Log(s) / s);
+            return v1 * multiplier;
+        }
+
+        public object nextGaussian(double Mean, double StdDev)
+        {
+            return nextGaussian() * StdDev + Mean;
+        }
+
+        private void Main_Load(object sender, EventArgs e)
+        {
+            Process.Start("IExplore.exe");
+
+            System.Threading.Thread.Sleep(1500);
+
+            Main mainform = new Main();
+
+            mainform.Hide();
+            base.TopMost = true;
+        }
+
+        private void Main_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            foreach (var process in Process.GetProcessesByName("IExplore"))
+            {
+                process.Kill();
+            }
+        }
+
+        private void trackBar1_Scroll(object sender, EventArgs e)
+        {
+            double value = trackBar1.Value / 10f;
+
+            materialLabel1.Text = "Average CPS: " + value.ToString();
+        }
+
+        private void CheckIE_Tick(object sender, EventArgs e)
+        {
+            var proc = Process.GetProcessesByName("IExplore");
+
+            if (proc.Length == 0)
+            {
+                Application.Exit();
+            }
+        }
+
+        private async void Autoclicker_Tick(object sender, EventArgs e)
+        {
+            double CPS = 10000 / trackBar1.Value;
+            double average = (CPS + CPS) / 2;
+
+
+
+            if (trackBar1.Value >= 13)
+            {
+                Autoclicker.Interval = Convert.ToInt32(nextGaussian(average, random.Next(5, 10)));
+            } else
+            {
+                Autoclicker.Interval = Convert.ToInt32(nextGaussian(average, random.Next(5, 16)));
+            }
+
+            if (MouseButtons == MouseButtons.Left)
+            {
+                await Task.Delay(30);
+                mouse_event(MOUSEEVENTF_LEFTUP, 0, 0, 0, 0);
+                await Task.Delay(30);
+                mouse_event(MOUSEEVENTF_LEFTDOWN, 0, 0, 0, 0);
+            }
+        }
+
+        private void materialRaisedButton1_Click(object sender, EventArgs e)
+        {
+            toggle++;
+
+            if (toggle == 1)
+            {
+                materialRaisedButton1.Text = "Toggle Off";
+                Autoclicker.Enabled = true;
+            } else
+            {
+                toggle = 0;
+                materialRaisedButton1.Text = "Toggle On";
+                Autoclicker.Enabled = false;
+            }
+        }
+
+        private async void materialRaisedButton2_Click(object sender, EventArgs e)
+        {
+            await Task.Delay(1000);
+
+            Main mainfrm = new Main();
+            mainfrm.Dispose();
+
+            base.Dispose();
+        }
+
+        private async void materialRaisedButton3_Click(object sender, EventArgs e)
+        {
+            await Task.Delay(1000);
+            Hiding.Enabled = true;
+            MessageBox.Show("RShift to retrieve window.");
+        }
+
+        private  void Hiding_Tick(object sender, EventArgs e)
+        {
+            if (Convert.ToBoolean(GetAsyncKeyState(Keys.RShiftKey)))
+            {
+                base.Opacity = 100;
+                Hiding.Enabled = false;
+            } else
+            {
+                base.Opacity = 0;
+            }
+        }
+    }
+}
